c2d1460e51da9b70c931733b5db7cf49d1de0a2d
BMO <bmo@wpengine.com>
2017-05-08 11:58:25 -0500

    Plugin Setup

---
+++ bmo-google-oauth2.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+Plugin Name: BMO Google OAuth2
+Description: Google OAuth2 Plugin
+Version: 0.1
+Author: BMO ^_^
+*/
+
+require_once 'google-api-php-client/vendor/autoload.php'; //Require Google API PHP Client
+
+/***** The Master Class to Rule them All *****/
+class bmo_google_oath {
+
+	public function __contsruct(){
+
+	}
+
+}
