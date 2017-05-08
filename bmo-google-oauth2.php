e533cf7fa90ddc879055a60abd70766383c35f3a
BMO <bmo@wpengine.com>
2017-05-08 16:16:02 -0500

    v0.4.6 Working on actual Google OAuth Part

--- bmo-google-oauth2.php
+++ bmo-google-oauth2.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: BMO Google OAuth2
 Description: Google OAuth2 Plugin
-Version: 0.4.5
+Version: 0.4.6
 Author: BMO ^_^
 */
 
--- src/bmo-auth.php
+++ src/bmo-auth.php
@@ -1,13 +1,13 @@
 <?php
 
-	require_once 'vendor/google-api-php-client/vendor/autoload.php'; //Require Google API PHP Client
+	require_once __DIR__ . '/../vendor/google-api-php-client/vendor/autoload.php'; //Require Google API PHP Client
 
 	class bmo_auth extends bmo_google_oath {
 
 		private $google;
 		private $secret_key;
 		private $redirect_url;
-		private $auth_uri;
+		private $auth_ur;
 		private $token_uri;
 
 		public function init(){
@@ -16,12 +16,13 @@
 			$this->bmo_options->token_uri = "https://accounts.google.com/o/oauth2/token";
 			$this->bmo_options->auth_provider_x509_cert_url = "https://www.googleapis.com/oauth2/v1/certs";
 			$client_secrets = [
-				'web' => $this->bmo_options;
+				'web' => (array)$this->bmo_options
 			];
-			$client_secrest = json_encode( $client_secrets );
 			$this->google = new Google_Client();
 			$this->google->setAuthConfig( $client_secrets );
-			var_dump( $this->google );
+			$this->auth_url = filter_var( $this->google->createAuthUrl(), FILTER_SANITIZE_URL );
+			wp_redirect( $this->auth_url );
+			exit();
 		}
 
 	}
